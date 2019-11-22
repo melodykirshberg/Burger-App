62fae9a3096defc4a82e870df92637afaa3d30da
verlitas <55808608+verlitas@users.noreply.github.com>
2019-11-21 22:38:35 -0700

    adding comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,44 @@
+// Dependencies ==========================
 const express = require("express");
 const router = express.Router();
-const burger = require("../models/burger.js");
+// =======================================
 
+// Import burger.js ======================
+const burger = require("../models/burger.js");
+// =======================================
+
+// Routes ================================
 router.get("/", function (req, res) {
     burger.selectAll(function (data) {
-        let hbsObject = {
+        var hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
         res.render('index', hbsObject);
     });
 });
 
-router.post("/api/burgers", function (req, res) {
+router.post("/burgers", function (req, res) {
     burger.insertOne([
-        'name', 'devoured'],[req.body.name, req.body.devoured], function(result) {
-        res.json({ id: result.insertId });
+        'burger_name'
+    ], [
+        req.body.burger_name
+    ], function(result) {
+        res.redirect('/');
     });
  });
 
-router.put("/api/burgers/:id", function (req, res) { 
+router.put("/burgers/:id", function (req, res) { 
     const condition = 'id = ' + req.params.id;
-    console.log('condition', condition);
-    burger.updateOne({devoured: req.body.devoured}, condition, function(result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function(result) {
+        res.redirect('/');
     })
 });
-router.delete('/api/burgers/:id', function(req, res) {
-    const condition = 'id = ' + req.param.id;
+// =======================================
 
-    burger.delete(condition, function(result) {
-        if (result.affectedRows == 0) {
-            return res.status(404).end();
-        }
-    });
-});
-
+// Export to server.js ===================
 module.exports = router;
+// =======================================
+
